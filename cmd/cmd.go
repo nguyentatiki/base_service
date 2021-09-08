6a05cf2187e8cec08495166a7c2b835402cb9f8b
Nguyen Ta <nguyentt@techvn.mti.co.jp>
2021-09-08 11:51:11 +0700

    Change name + add dockerfile template

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,11 +8,8 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "start",
+	Short: "start the server",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
